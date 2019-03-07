0979a896a4761f157937ab30d65fa8819a73a88d
Krazune <krazune@gmail.com>
2019-03-07 10:18:32 +0000

    Add the sys_write system call

--- source/system.asm
+++ source/system.asm
@@ -4,6 +4,7 @@
 ; System calls
 SYS_EXIT	equ	1
 SYS_READ	equ	3
+SYS_WRITE	equ	4
 
 ; Status codes
 EXIT_SUCCESS	equ	0
@@ -27,6 +28,22 @@ sysRead:
 	mov		eax, SYS_READ
 	mov		ebx, [ebp + 8]	; file descriptor
 	mov		ecx, [ebp + 12]	; destination buffer
+	mov		edx, [ebp + 16]	; count
+	int		0x80
+
+	pop		ebx
+
+	leave
+	ret
+
+sysWrite:
+	enter	0, 0
+
+	push	ebx
+
+	mov		eax, SYS_WRITE
+	mov		ebx, [ebp + 8]	; file descriptor
+	mov		ecx, [ebp + 12]	; source buffer
 	mov		edx, [ebp + 16]	; count
 	int		0x80
 
