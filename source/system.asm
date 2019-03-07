ad000583bd3c8bd827c5f67871ae970251424dee
Krazune <krazune@gmail.com>
2019-03-07 10:08:48 +0000

    Add the sys_read system call

--- source/system.asm
+++ source/system.asm
@@ -3,14 +3,36 @@
 
 ; System calls
 SYS_EXIT	equ	1
+SYS_READ	equ	3
 
 ; Status codes
 EXIT_SUCCESS	equ	0
 EXIT_FAILURE	equ	1
 
+; Standard file descriptors
+STDIN	equ	0
+STDOUT	equ	1
+STDERR	equ	2
+
 sysExit:
 	mov	eax, SYS_EXIT
 	pop	ebx				; status code
 	int	0x80
+
+sysRead:
+	enter	0, 0
+
+	push	ebx
+
+	mov		eax, SYS_READ
+	mov		ebx, [ebp + 8]	; file descriptor
+	mov		ecx, [ebp + 12]	; destination buffer
+	mov		edx, [ebp + 16]	; count
+	int		0x80
+
+	pop		ebx
+
+	leave
+	ret
 
 %endif
