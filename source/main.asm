45692dcacb5cdbe25a63a40404bdf5808f788e8a
Krazune <krazune@gmail.com>
2019-03-09 11:16:36 +0000

    Add invalid argument count error message

--- source/main.asm
+++ source/main.asm
@@ -6,6 +6,9 @@ segment .data
 	helpMessage			db	'BFASM: Brainfuck interpreter for the IA-32 architecture.', 0xA, 'Usage: bfasm <path>', 0xA, 'Version: 2019.03.0', 0xA, 0
 	helpMessageLength	equ	$ - helpMessage
 
+	invalidArgumentCountMessage			db	'Invalid argument count.', 0xA, 'Usage: bfasm <path>', 0xA, 0
+	invalidArgumentCountMessageLength	equ	$ - invalidArgumentCountMessage
+
 segment .text
 _start:
 	cmp		dword [esp], 1			; Check if argument count is 1
@@ -13,6 +16,12 @@ _start:
 
 	cmp		dword [esp], 2			; Check if argument count is 2
 	je		_start.doubleArguments	; Call interpreter
+
+	push	invalidArgumentCountMessageLength
+	push	invalidArgumentCountMessage
+	push	STDOUT
+	call	sysWrite							; Print invalid argument count error message
+	add		esp, 12
 
 	call	failureExit
 
