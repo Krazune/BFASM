a74f3d1f2cf80134d25c75067894bb0386391055
Krazune <krazune@gmail.com>
2019-03-09 10:47:26 +0000

    Add program information print on single argument

---
+++ source/main.asm
@@ -0,0 +1,31 @@
+%include "system.asm"
+
+global _start
+
+segment .data
+	helpMessage			db	'BFASM: Brainfuck interpreter for the IA-32 architecture.', 0xA, 'Usage: bfasm <path>', 0xA, 'Version: 2019.03.0', 0xA, 0
+	helpMessageLength	equ	$ - helpMessage
+
+segment .text
+_start:
+	cmp		dword [esp], 1			; Check if argument count is 1
+	je		_start.singleArgument	; Print program information
+
+	call	successExit
+
+.singleArgument:
+	push	helpMessageLength
+	push	helpMessage
+	push	STDOUT
+	call	sysWrite
+	add		esp, 12
+
+	call	successExit
+
+successExit:
+	push	EXIT_SUCCESS
+	call	sysExit
+
+failureExit:
+	push	EXIT_FAILURE
+	call	sysExit
